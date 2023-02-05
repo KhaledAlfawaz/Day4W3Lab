1ffe400906f75374e90b00799d2605a954924e1f
KhaledAlfawaz <khaled@al-fawaz.net>
2023-02-05 05:10:35 +0300

    add routing

---
+++ src/component/About.tsx
@@ -0,0 +1,12 @@
+import { Center, Heading } from '@chakra-ui/react'
+import React from 'react'
+
+function About() {
+  return (
+    <Center>
+        <Heading>About</Heading>
+        </Center>
+  )
+}
+
+export default About
--- src/component/Nav.tsx
+++ src/component/Nav.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from "react";
+import { ReactNode } from 'react';
 import {
   Box,
   Flex,
@@ -16,27 +16,12 @@ import {
   useColorModeValue,
   Stack,
   useColorMode,
-} from "@chakra-ui/react";
-import { HamburgerIcon, CloseIcon, AddIcon } from "@chakra-ui/icons";
-import { MoonIcon, SunIcon } from "@chakra-ui/icons";
-import { IoLogoJavascript } from "react-icons/all";
+} from '@chakra-ui/react';
+import { HamburgerIcon, CloseIcon, AddIcon } from '@chakra-ui/icons';
+import { MoonIcon, SunIcon } from '@chakra-ui/icons';
+import { IoLogoJavascript } from 'react-icons/all';
 
-const Links = ["Posts", "About", "Owner"];
-
-const NavLink = ({ children }: { children: ReactNode }) => (
-  <Link
-    px={2}
-    py={1}
-    rounded={"md"}
-    _hover={{
-      textDecoration: "none",
-      bg: useColorModeValue("gray.200", "gray.700"),
-    }}
-    href={"#"}
-  >
-    {children}
-  </Link>
-);
+const Links = ['Home', 'About', 'Owner'];
 
 export default function withAction() {
   const { isOpen, onOpen, onClose } = useDisclosure();
@@ -44,39 +29,61 @@ export default function withAction() {
 
   return (
     <>
-      <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
-        <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
+      <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
+        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
           <IconButton
-            size={"md"}
+            size={'md'}
             icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
-            aria-label={"Open Menu"}
-            display={{ md: "none" }}
+            aria-label={'Open Menu'}
+            display={{ md: 'none' }}
             onClick={isOpen ? onClose : onOpen}
           />
-          <HStack spacing={8} alignItems={"center"}>
-          <Box>My Blog</Box>
+          <HStack spacing={8} alignItems={'center'}>
+            <Box>My Blog</Box>
             <HStack
-              as={"nav"}
+              as={'nav'}
               spacing={4}
-              display={{ base: "none", md: "flex" }}
+              display={{ base: 'none', md: 'flex' }}
             >
               {Links.map((link) => (
-                <NavLink key={link}>{link}</NavLink>
+                <Link
+                  px={2}
+                  py={1}
+                  rounded={'md'}
+                  _hover={{
+                    textDecoration: 'none',
+                    bg: useColorModeValue('gray.200', 'gray.700'),
+                  }}
+                  href={`/${link}`}
+                >
+                  {link}
+                </Link>
               ))}
             </HStack>
           </HStack>
-          <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
+          <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
             <Button onClick={toggleColorMode}>
-              {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
+              {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
             </Button>
           </Flex>
         </Flex>
 
         {isOpen ? (
-          <Box pb={4} display={{ md: "none" }}>
-            <Stack as={"nav"} spacing={4}>
+          <Box pb={4} display={{ md: 'none' }}>
+            <Stack as={'nav'} spacing={4}>
               {Links.map((link) => (
-                <NavLink key={link}>{link}</NavLink>
+                <Link
+                  px={2}
+                  py={1}
+                  rounded={'md'}
+                  _hover={{
+                    textDecoration: 'none',
+                    bg: useColorModeValue('gray.200', 'gray.700'),
+                  }}
+                  href={`/${link}`}
+                >
+                  {link}
+                </Link>
               ))}
             </Stack>
           </Box>
